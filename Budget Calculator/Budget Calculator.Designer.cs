2b4d6d18c2aa8414400c3e95bf7d454c153908fc
kr106 <kathleenreq@gmail.com>
2021-08-09 08:36:04 +1200

    Add project files.

---
+++ Budget Calculator/Budget Calculator.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Budget_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBoxBalance = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelGroupBy = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.Location = new System.Drawing.Point(38, 55);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(114, 27);
+            this.buttonUpload.TabIndex = 1;
+            this.buttonUpload.Text = "Upload Transactions";
+            this.buttonUpload.UseVisualStyleBackColor = true;
+            this.buttonUpload.Click += new System.EventHandler(this.buttonUpload_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Date});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(304, 55);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(307, 326);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBoxBalance
+            // 
+            this.textBoxBalance.Location = new System.Drawing.Point(122, 193);
+            this.textBoxBalance.Name = "textBoxBalance";
+            this.textBoxBalance.ReadOnly = true;
+            this.textBoxBalance.Size = new System.Drawing.Size(84, 20);
+            this.textBoxBalance.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Balance";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(172, 55);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(70, 27);
+            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(121, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // labelGroupBy
+            // 
+            this.labelGroupBy.AutoSize = true;
+            this.labelGroupBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGroupBy.Location = new System.Drawing.Point(35, 120);
+            this.labelGroupBy.Name = "labelGroupBy";
+            this.labelGroupBy.Size = new System.Drawing.Size(63, 16);
+            this.labelGroupBy.TabIndex = 7;
+            this.labelGroupBy.Text = "Group by";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelGroupBy);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxBalance);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.buttonUpload);
+            this.Name = "Form1";
+            this.Text = "Budget Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.TextBox textBoxBalance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelGroupBy;
+    }
+}
+
---
+++ Budget Calculator/Budget Calculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Budget_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonUpload_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            StreamReader reader;
+            string[] transactions;
+            const string FILTER = "CSV Files|*.csv|All Files|*.*";
+            string line = "", date = "", payee = "", description = "", 
+                reference = "", particulars = "";
+            decimal amount = 0, balance = 0;
+
+            //Set filter for dialog control
+            openFileDialog1.Filter = FILTER;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Open file
+                reader = File.OpenText(openFileDialog1.FileName);
+
+                //Read values
+                while (!reader.EndOfStream)
+                {
+                    try
+                    {
+                        //Read a line from the file
+                        line = reader.ReadLine();
+                        //Split values from the line
+                        transactions = line.Split(',');
+                        if (transactions[0] != "Date")
+                        {
+                            //Extract values into separate variables
+                            date = transactions[0];
+                            amount = decimal.Parse(transactions[1]);
+                            payee = transactions[2];
+                            description = transactions[3];
+                            reference = transactions[4];
+                            particulars = transactions[5]; //Only gets first line of particulars
+
+                            //Calculate balance
+                            balance += amount;
+                            //Display balance
+                            textBoxBalance.Text = balance.ToString("c");
+                            //Change colour of textbox according to balance
+                            if (balance < 0) { textBoxBalance.BackColor = Color.Red; } else { textBoxBalance.BackColor = Color.Green;  }
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error: " + line); //Display error message
+                    }
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// Closes the applicaiton.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
